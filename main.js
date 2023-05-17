75bc8de43a3a51d913e223b0893f2c912f00394b
filipzlatojevic <ficozlatojevic@gmail.com>
2023-05-17 21:08:39 +0200

    finish

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-import "./style.css";
+import './style.css';
 /** @type {HTMLCanvasElement} */
-const canvas = document.querySelector("#canvas");
-let context = canvas.getContext("2d");
+const canvas = document.querySelector('#canvas');
+let context = canvas.getContext('2d');
 
 var window_height = window.innerHeight;
 var window_width = window.innerWidth;
 
-// /** @type {CanvasRenderingContext2D} */
 canvas.width = window_width;
 canvas.height = window_height;
-canvas.style.background = "orange";
-
-let hit_counter = 0;
+canvas.style.background = 'orange';
 
 class Circle {
   constructor(xpos, ypos, radius, color, text, speed) {
@@ -30,9 +27,9 @@ class Circle {
     context.beginPath();
 
     context.strokeStyle = this.color;
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.font = "20px Arial";
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.font = '20px Arial';
     context.fillText(this.text, this.xpos, this.ypos);
     // context.strokeText(this.text, this.xpos, this.ypos);
 
@@ -43,30 +40,22 @@ class Circle {
   }
 
   update() {
-    this.text = hit_counter;
-
-    context.clearRect(0, 0, window_width, window_height);
-
     this.draw(context);
 
     if (this.xpos + this.radius > window_width) {
       this.dx = -this.dx;
-      hit_counter++;
     }
 
     if (this.xpos - this.radius < 0) {
       this.dx = -this.dx;
-      hit_counter++;
     }
 
     if (this.ypos - this.radius < 0) {
       this.dy = -this.dy;
-      hit_counter++;
     }
 
     if (this.ypos + this.radius > window_height) {
       this.dy = -this.dy;
-      hit_counter++;
     }
 
     this.xpos += this.dx;
@@ -74,13 +63,38 @@ class Circle {
   }
 }
 
-let my_circle = new Circle(100, 100, 50, "black", hit_counter, 8);
+let getDistance = function (xpos1, ypos1, xpos2, ypos2) {
+  var result = Math.sqrt(Math.pow(xpos2 - xpos1, 2) + Math.pow(ypos2 - ypos1, 2));
+  return result;
+};
 
-my_circle.draw(context);
+let my_circle1 = new Circle(600, 500, 50, 'black', 'a', 5);
+let my_circle2 = new Circle(300, 300, 200, 'black', 'b', 0);
+
+my_circle1.draw(context);
+my_circle2.draw(context);
 
 let updateCircle = function () {
   requestAnimationFrame(updateCircle);
-  my_circle.update();
+  context.clearRect(0, 0, window_width, window_height);
+  my_circle1.update();
+  my_circle2.update();
+
+  if (
+    getDistance(my_circle1.xpos, my_circle1.ypos, my_circle2.xpos, my_circle2.ypos) <
+    my_circle2.radius + my_circle1.radius
+  ) {
+    my_circle2.color = 'red';
+  }
+
+  if (
+    getDistance(my_circle1.xpos, my_circle1.ypos, my_circle2.xpos, my_circle2.ypos) >=
+    my_circle2.radius + my_circle1.radius
+  ) {
+    my_circle2.color = 'black';
+  }
 };
 
 updateCircle();
+
+console.log(getDistance(my_circle1.xpos, my_circle1.ypos, my_circle2.xpos, my_circle2.ypos));
