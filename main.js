8122e271a5e150fa82b5c9312748c066469315c5
filipzlatojevic <ficozlatojevic@gmail.com>
2023-05-17 19:32:51 +0200

    finish

--- main.js
+++ main.js
@@ -11,13 +11,19 @@ canvas.width = window_width;
 canvas.height = window_height;
 canvas.style.background = "orange";
 
+let hit_counter = 0;
+
 class Circle {
-  constructor(xpos, ypos, radius, color, text) {
+  constructor(xpos, ypos, radius, color, text, speed) {
     this.xpos = xpos;
     this.ypos = ypos;
     this.radius = radius;
     this.color = color;
     this.text = text;
+    this.speed = speed;
+
+    this.dx = 1 * this.speed;
+    this.dy = 1 * this.speed;
   }
 
   draw(context) {
@@ -35,19 +41,46 @@ class Circle {
     context.stroke();
     context.closePath();
   }
+
+  update() {
+    this.text = hit_counter;
+
+    context.clearRect(0, 0, window_width, window_height);
+
+    this.draw(context);
+
+    if (this.xpos + this.radius > window_width) {
+      this.dx = -this.dx;
+      hit_counter++;
+    }
+
+    if (this.xpos - this.radius < 0) {
+      this.dx = -this.dx;
+      hit_counter++;
+    }
+
+    if (this.ypos - this.radius < 0) {
+      this.dy = -this.dy;
+      hit_counter++;
+    }
+
+    if (this.ypos + this.radius > window_height) {
+      this.dy = -this.dy;
+      hit_counter++;
+    }
+
+    this.xpos += this.dx;
+    this.ypos += this.dy;
+  }
 }
 
-let all_circles = [];
+let my_circle = new Circle(100, 100, 50, "black", hit_counter, 8);
 
-let createCircle = function (circle) {
-  circle.draw(context);
+my_circle.draw(context);
+
+let updateCircle = function () {
+  requestAnimationFrame(updateCircle);
+  my_circle.update();
 };
 
-for (var numbers = 0; numbers < 10; numbers++) {
-  let random_x = Math.random() * window_width;
-  let random_y = Math.random() * window_height;
-
-  let my_circle = new Circle(random_x, random_y, 50, "black", numbers + 1);
-  all_circles.push(my_circle);
-  createCircle(all_circles[numbers]);
-}
+updateCircle();
