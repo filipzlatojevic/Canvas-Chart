7289535672458da954a0a77b509a92022cec1b21
filipzlatojevic <ficozlatojevic@gmail.com>
2023-05-17 18:41:33 +0200

    finish

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
 import "./style.css";
-
+/** @type {HTMLCanvasElement} */
 const canvas = document.querySelector("#canvas");
-
 let context = canvas.getContext("2d");
-
-canvas.style.background = "#ffffff";
 
 var window_height = window.innerHeight;
 var window_width = window.innerWidth;
 
+// /** @type {CanvasRenderingContext2D} */
 canvas.width = window_width;
 canvas.height = window_height;
+canvas.style.background = "#ff8";
 
-context.fillRect(300, 0, 100, 100);
+class Circle {
+  constructor(xpos, ypos, radius, color) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.radius = radius;
+    this.color = color;
+  }
 
-context.fillStyle = "red";
-context.fillRect(100, 500, 100, 100);
+  draw(context) {
+    context.beginPath();
+    context.lineWidth = 5;
+    context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+    context.stroke();
+    context.closePath();
+  }
+}
 
-context.beginPath();
-context.strokeStyle = "blue";
-context.lineWidth = 20;
-context.arc(100, 100, 50, 0, Math.PI * 2, false);
-context.stroke();
-context.closePath();
+let all_circles = [];
+
+let createCircle = function (circle) {
+  circle.draw(context);
+};
+
+for (var numbers = 0; numbers < 10; numbers++) {
+  let random_x = Math.random() * window_width;
+  let random_y = Math.random() * window_height;
+
+  let my_circle = new Circle(random_x, random_y, 50, "black");
+  all_circles.push(my_circle);
+  createCircle(all_circles[numbers]);
+}
